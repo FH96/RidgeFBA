7fa2f47c1f161d0d1b049e2e35f003b66439fc14
Fatemeh Heydari <102415660+FH96@users.noreply.github.com>
2022-11-21 05:21:39 -0800

    Update Ridge FBA test .jl

--- Ridge FBA test .jl
+++ Ridge FBA test .jl
@@ -1,15 +1,18 @@
+#In this script the metabolic model 'e_coli_core' is  downloaded and by assumption of biomass as objective function,
+#flux dixtribution is computed and the value for objective function is tested
+
 using JuMP, Ipopt
 using Random
 using SBML
 using HTTP
 using Test
 
-
+#Downloading the metabolic model
 ecoli_model=HTTP.get("http://bigg.ucsd.edu/static/models/e_coli_core.xml")
 write("e_coli_core.xml",ecoli_model.body)
 ecoli_metabolic_model=readSBML("e_coli_core.xml")
 
-
+#Obtaining the biomass reaction inedx
 for n in 1:length(reactions(ecoli_metabolic_model))
     if occursin("BIOMASS" , reactions(ecoli_metabolic_model)[n])
         global biomass_index=n
@@ -17,8 +20,8 @@ for n in 1:length(reactions(ecoli_metabolic_model))
 end
 
 
+#As biomass is set as objective function the corresponding element in in c vector is set to 1
 c_test=zeros(1,length(reactions(ecoli_metabolic_model)))
-
 c_test[biomass_index]=1
 
 
