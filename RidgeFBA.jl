b87df6705eee97e0c661c5f1ecf75f8605f1ef54
Fatemeh Heydari <102415660+FH96@users.noreply.github.com>
2022-12-22 00:00:10 -0800

    Update RidgeFBA.jl

--- RidgeFBA.jl
+++ RidgeFBA.jl
@@ -1,6 +1,42 @@
+#-------------------------------------------------------------------------------------------
+#=
+    Purpose:    Implementation for Ridge FBA 
+    Authors:    Fatemeh Heydari - University of Tehran
+                Mojtaba Tefagh  - Sharif University of Technology 
+    Date:       December 2022
+=#
+
+#-------------------------------------------------------------------------------------------
+
 """
 Ridge FBA optimizes the desired objective function while minimizing L2 norm of the flux vector
 The flux vector computed by Ridge FBA is more consistent with the actual values
+
+
+# INPUTS
+
+- metabolic model:  Metabolic models contain sotoichiometric matrix  and also other informations such as flux boundaries 
+                    and Gene-Protein-Reaction rules. They can be found in different formats including .xml.
+                    Metabolic models can be downloaded from [BiGG Models](http://bigg.ucsd.edu/) or elsewhere.
+
+- lambda:           Regularization coefficient for the L2 norm term in the objective function of the optimization problem.
+                    The larger lambda, the smaller the flux vector.
+
+- c:                A vector with the same length as the metabolic model reactions, determining the objective function.
+                    For example, if biomass is meant to be the objective function, the corresponding element in c vector
+                    is set to 1 and the others are zero. 
+ 
+
+# OUTPUTS
+
+- flux_vector:      It is the calculated flux distribution, which is of type Vector{Float64}
+                    (a vector whose elements are Float64), So this can be indexed and used like any other vector. 
+
+
+# EXAMPLES
+
+flux_vector=Ridge_FBA(ecoli_metabolic_model,c_test,10^-5)                                                          
+
 """
 
 using JuMP, Ipopt , SBML
